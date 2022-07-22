c053cc07fb90f77204ac8981057d3311ad9a7ce9
shubham bramhane <59164754+shubham-bramhane@users.noreply.github.com>
2022-07-22 22:59:10 +0530

    22-07-22
    html pages

---
+++ introhtml.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>
+
+<?php
+
+
+
+
+// $list=array('chandrapur','ghugus','wani');
+
+// for ($i=0;$i<count($list);$i++){
+//     echo $list[$i]."<br>";
+// }
+
+
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
+
+
+
+
+
+
+
+
+
+
+// for ($i=0;$i<100;$i++){
+//     // echo $i."<br>";
+//     if ($i%2==0){
+//         echo "even->".$i."<br>";
+//     }
+//     else{
+//         echo "odd->".$i."<br>";
+//     }
+// }
+
+
+
+// for ($i=0;$i<10;$i++){
+    
+//     for ($j=0;$j<=$i;$j++){
+//         echo "*  ";
+//     }
+//     echo "<br>";
+// }
+
+
+
+
+
+// for ($i=10;$i>0;$i--){
+    
+//     for ($j=0;$j<=$i;$j++){
+//         echo "*  ";
+//     }
+//     echo "<br>";
+// }
+
+
+
+
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+for($i=0;$i<10;$i++){
+$sum=10;
+for ($j=0;$j<=$i;$j++){
+    echo $sum." ";
+    $sum=$sum+10; 
+}
+echo "<br>";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+    </h1>
+</body>
+</html>
